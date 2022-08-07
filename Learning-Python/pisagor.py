54ac04114307250d0f537e017b5d880841b7d956
mert <yurekli.mertt@icloud.com>
2022-08-07 20:16:55 +0300

    updated

--- Learning-Python/pisagor.py
+++
@@ -1,9 +0,0 @@
-edge1 = float(input("First edge: "))
-edge2 = float(input("Second edge: "))
-
-def pisagor(edge1, edge2):
-    pisagor = (edge1**2 + edge2**2) ** 0.5
-
-    return pisagor
-
-print(pisagor(edge1, edge2))
