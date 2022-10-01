cb92bac399f86cce4ba11a362de58a2bcaed9fbe
mert <yurekli.mertt@icloud.com>
2022-10-01 11:41:34 +0300

    Updated

--- Draw/main.py
+++ Draw/main.py
@@ -6,9 +6,7 @@ painter = Turtle()
 painter.shape("turtle")
 painter.color("green")
 painter.speed("fastest")
-
 turtle.colormode(255)
-#painter.pensize(10)
 
 for _ in range(72):
     painter.pencolor(randint(1, 255), randint(1, 255), randint(1, 255))
